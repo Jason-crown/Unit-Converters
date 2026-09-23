805f0c463a5ef6e0dd99a801b35fdcce59cbd5ca
Jason Klutse <jklutse@uoguelph.ca>
2026-09-22 22:13:47 -0400

    general main file to start the conversion

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package java_converter.src;
+public class Main {
+    public static void main(String[] args) {
+        UnitConverter converter = new UnitConverter();
+        converter.start();
+    }
+}
